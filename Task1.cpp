a847b525ceaf2bbd34ce41613a9ba1076cc04404
Alex Shevtsov <Shavo4kin@gmail.com>
2015-09-20 12:34:02 +0300

    Added tests for Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -15,7 +15,18 @@ double FirstSinus(double x, double e)
 	}
 	return sinus;
 }
+void Test(double x, double e, double rightsin)
+{
+	double sinus = FirstSinus(x, e);
+	if (fabs(sinus - rightsin) < e)
+		cout << "OK" << endl;
+	else
+		cout << "False" << endl;
+}
 void main()
 {
-
+	Test(4, 0.05, sin(4));
+	Test(7.3, 0.1, sin(7.3));
+	Test(2, 0.6, sin(2));
+	Test(1.1, 0.025, sin(1.1));
 }
